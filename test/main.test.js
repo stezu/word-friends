70e81121658864db75cf62cd78eeb597b03851cc
Stephen Zuniga <stephen.zuniga001@gmail.com>
2016-09-10 17:39:06 -0400

    add a large input test case
    using /usr/share/dict/words as a source

--- test/main.test.js
+++ test/main.test.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const expect = require('chai').expect;
 
 const inputFile = path.resolve('./files', 'input.txt');
+const largeInputFile = path.resolve('./files', 'largeinput.txt');
 
 const testCases = new Map();
 
@@ -14,18 +15,11 @@ testCases.set('node-stream', ['node', 'javascript/lib/cli.js', '--stream']);
 testCases.set('ruby', ['ruby', 'ruby/main.rb']);
 
 describe('[Main]', () => {
-  const expectedResults = [
-    1,
-    4,
-    1,
-    0,
-    1,
-    0
-  ];
 
   function test({
     command,
     args,
+    expected,
     done
   }) {
     const child = spawn(command, args);
@@ -52,7 +46,7 @@ describe('[Main]', () => {
           return !isNaN(item);
         });
 
-      expect(results.toString()).to.equal(expectedResults.toString());
+      expect(results.toString()).to.equal(expected.toString());
 
       done();
     });
@@ -60,29 +54,82 @@ describe('[Main]', () => {
     return child;
   }
 
-  testCases.forEach((command, testName) => {
-    const testPath = path.resolve('.', command[1]);
+  describe('input.txt', () => {
+    const expected = [
+      1,
+      4,
+      1,
+      0,
+      1,
+      0
+    ];
 
-    describe(`#${testName}`, () => {
+    testCases.forEach((command, testName) => {
+      const testPath = path.resolve('.', command[1]);
 
-      it('succeeds with a filepath argument', (done) => {
-        test({
-          command: command[0],
-          args: [testPath, inputFile].concat(command.slice(2)),
-          done
+      describe(`#${testName}`, () => {
+
+        it('succeeds with a filepath argument', (done) => {
+          test({
+            command: command[0],
+            args: [testPath, inputFile].concat(command.slice(2)),
+            expected,
+            done
+          });
+        });
+
+        it('succeeds when a file is piped in', (done) => {
+          const file = fs.createReadStream(inputFile);
+
+          const child = test({
+            command: command[0],
+            args: [testPath].concat(command.slice(2)),
+            expected,
+            done
+          });
+
+          file.pipe(child.stdin);
         });
       });
+    });
+  });
 
-      it('succeeds when a file is piped in', (done) => {
-        const file = fs.createReadStream(inputFile);
+  describe('largeinput.txt', () => {
+    const expected = [
+      4,
+      19,
+      0,
+      24,
+      29,
+      13
+    ];
 
-        const child = test({
-          command: command[0],
-          args: [testPath].concat(command.slice(2)),
-          done
+    testCases.forEach((command, testName) => {
+      const testPath = path.resolve('.', command[1]);
+
+      describe(`#${testName}`, () => {
+
+        it('succeeds with a filepath argument', (done) => {
+          test({
+            command: command[0],
+            args: [testPath, largeInputFile].concat(command.slice(2)),
+            expected,
+            done
+          });
         });
 
-        file.pipe(child.stdin);
+        it('succeeds when a file is piped in', (done) => {
+          const file = fs.createReadStream(largeInputFile);
+
+          const child = test({
+            command: command[0],
+            args: [testPath].concat(command.slice(2)),
+            expected,
+            done
+          });
+
+          file.pipe(child.stdin);
+        });
       });
     });
   });
